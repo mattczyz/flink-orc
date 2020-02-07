c75818817c955905eaf59c9d1956cbf6f9fbea98
mattczyz <matt.czyz@gmail.com>
2020-02-07 13:47:16 +0000

    Move Encoder specific code to its own package

--- src/main/java/uk/co/realb/flink/orc/OrcBuilder.java
+++
@@ -1,9 +0,0 @@
-package uk.co.realb.flink.orc;
-
-import org.apache.hadoop.fs.FileSystem;
-import uk.co.realb.flink.orc.encoder.EncoderOrcWriter;
-import java.io.IOException;
-
-public interface OrcBuilder<T> {
-    EncoderOrcWriter<T> createWriter(FileSystem stream) throws IOException;
-}
